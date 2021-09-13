9dd02da54f9d9971321b0bf8cc5ca65904d7f4a3
DheVam <vanshidheeraj07@gmail.com>
2021-09-13 10:59:54 +0530

    validations added

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-const success = () => {
-   alert("success")
-}
+const peru =  document.getElementById("name");
+const email = document.getElementById("mail");
+const button = document.getElementById("button");
+const errors = document.getElementById("error");
+const form = document.getElementById("form");
+
+form.addEventListener('submit',(e) => {
+   let messages = [];
+   if(peru.value === '' || peru.value === null){
+      messages.push("Name field cannot be empty");
+   }
+   if(email.length === 0 || email.value === ''){
+      messages.push("Email field cannot be empty");
+   }
+   if(messages.length > 0){
+      e.preventDefault()
+      errors.innerText = messages.join(', ')
+   }else{
+      alert("success");
+   }
+})
